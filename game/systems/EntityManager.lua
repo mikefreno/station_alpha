f63b20f68faa3d4ca397adc81a5c965990813c46
Michael Freno <michaelt.freno@gmail.com>
2025-09-02 14:19:46 -0400

    refactored to simplify ecs. Need better map struct

--- game/systems/EntityManager.lua
+++ game/systems/EntityManager.lua
@@ -8,55 +8,69 @@ local EntityManager = {}
 EntityManager.__index = EntityManager
 
 function EntityManager.new()
-	local self = setmetatable({}, EntityManager)
-	self.entities = {}
-	self.components = {}
-	return self
+  local self = setmetatable({}, EntityManager)
+  self.entities = {}
+  self.components = {}
+  return self
 end
 
 function EntityManager:createEntity()
-	local id = #self.entities + 1
-	self.entities[id] = true
-	return id
+  local id = #self.entities + 1
+  self.entities[id] = true
+  return id
 end
 
 ---@param entityId integer
 ---@param type ComponentType
 ---@param data any
 function EntityManager:addComponent(entityId, type, data)
-	if not self.components[type] then
-		self.components[type] = {}
-	end
-	self.components[type][entityId] = data
+  if not self.components[type] then
+    self.components[type] = {}
+  end
+  self.components[type][entityId] = data
 end
 
 ---@param type ComponentType
 ---@param data any
 ---@return integer?
 function EntityManager:find(type, data)
-	local compTable = self.components[type]
-	if not compTable then
-		return nil
-	end
+  local byType = self.components[type]
+  if not byType then
+    return nil
+  end
 
-	for id, comp in pairs(compTable) do
-		if type == ComponentType.POSITION and comp.x == data.x and comp.y == data.y then
-			return id
-		end
-	end
+  for id, comp in pairs(byType) do
+    if type == ComponentType.POSITION then
+      if comp.x == data.x and comp.y == data.y then
+        return id
+      end
+    end
+  end
 
-	return nil
+  return nil
+end
+
+---@param type ComponentType.POSITION
+---@param data Vec2
+---@return integer?
+function EntityManager:findNearest(type, data)
+  local xdif = math.huge
+  local ydif = math.huge
+  local nearest = -1
+  local byType = self.components[type]
+  for id, component in pairs(byType) do
+  end
 end
 
 ---@param entityId integer
 ---@param type ComponentType
 ---@return unknown
 function EntityManager:getComponent(entityId, type)
-	local byType = self.components[type]
-	if not byType then
-		return nil
-	end
-	return byType[entityId]
+  local byType = self.components[type]
+  if not byType then
+    return nil
+  end
+  return byType[entityId]
 end
 
 return EntityManager.new()
--- game/systems/PathFinder.lua
+++ game/systems/PathFinder.lua
@@ -9,15 +9,20 @@ PathFinder.__index = PathFinder
 
 --- Simple A* implementation
 function PathFinder.new()
-	return setmetatable({}, PathFinder)
+  return setmetatable({}, PathFinder)
 end
 
 --- @param entityManager EntityManager
---- @param startEntity integer  -- entity that should start moving
---- @param goalEntity integer|Vec2   -- entity that represents the target (must have POSITION) or Vec2 Position for that entity
+--- @param start Vec2
+--- @param target Vec2
 --- @return table
-function PathFinder:findPath(entityManager, startEntity, goalEntity)
-	local startingNode = entityManager:getComponent(startEntity, ComponentType.MAPCELL)
+function PathFinder:findPath(entityManager, start, target)
+  local startingNode = entityManager:find(ComponentType.MAPNODE, start)
+  Logger:debug(startingNode)
+  --if startingNode then
+  --Logger:error("Missing startingNode for PathFinder")
+  return {}
+  --end
 end
 
 return PathFinder.new()
