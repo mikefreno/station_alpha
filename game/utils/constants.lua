075704b328dbdffb07bd3644a6c2457f4d8d6c9b
Michael Freno <michaelt.freno@gmail.com>
2025-09-03 13:16:48 -0400

    changes to streamline calculations

--- game/utils/constants.lua
+++ game/utils/constants.lua
@@ -1,5 +1,13 @@
-local TILE_SIZE = 25
+local MAP_W, MAP_H = 100, 75 -- logical grid size
+local LOGICAL_TILE_SIZE = 1 -- 1 logical unit == 1 tile
+local LOGICAL_TO_PIXEL = 32 -- 32 pixels per tile (default)
+
+-- this will be recomputed on resize
+local pixelSize = LOGICAL_TO_PIXEL
 
 return {
-    TILE_SIZE = TILE_SIZE
+  MAP_W = MAP_W,
+  MAP_H = MAP_H,
+  LOGICAL_TILE_SIZE = LOGICAL_TILE_SIZE,
+  pixelSize = pixelSize,
 }
