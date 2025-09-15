cbc5f5fa1ac3d7d7bfe94b87dcf9e7acf7f26752
Michael Freno <michaelt.freno@gmail.com>
2025-09-14 22:10:41 -0400

    sync

--- game/components/RightClickMenu.lua
+++ game/components/RightClickMenu.lua
@@ -114,6 +114,7 @@ function RightClickMenu:addMoveTo(entity)
   Gui.Button.new({
     parent = self.window,
     background = Color.new(0.2, 0.7, 0.7, 0.9),
+    px = 4,
     text = "Go To: " .. self.gridPosition.x .. "," .. self.gridPosition.y,
     callback = GoTo,
     positioning = Positioning.FLEX,
